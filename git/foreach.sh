7ce09b026e25bb25f2428ee7c50095d95797f4e5
Donald Curtis <dcurtis@milkbox.net>
2012-08-19 13:55:44 -0500

    updates to foreach.sh script

--- git/foreach.sh
+++ git/foreach.sh
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]; then
     exit -1
 fi
 
-for repo in `find ./ -maxdepth 1 -mindepth 1 -printf "%P\n"`
+for repo in `find ./ -maxdepth 1 -mindepth 1 | xargs basename`
 do
     cd $repo
     $@
